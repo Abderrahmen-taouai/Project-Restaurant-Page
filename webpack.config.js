a4510a2bae87848058c238481f3aa007d83fa66c
Tittou10 <prepa.issat@gmail.com>
2024-01-25 18:46:03 +0100

    The whole pages

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,16 @@ module.exports = {
 
 
   },
+    module: {
+        rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+    },
 };
