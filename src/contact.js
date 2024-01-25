06a69eda464bf720ae0dc1d3b74492571f1fa69d
Tittou10 <prepa.issat@gmail.com>
2024-01-25 12:50:02 +0100

    tabs  & Menu

--- src/contact.js
+++ src/contact.js
@@ -7,9 +7,16 @@ export const contacts= function () {
     head.textContent="Please feel free to contact us!";
     contact.appendChild(head);
     const para=  document.createElement('p');
-    para.textContent="We are located at  St 415 Med Ali  Gabes Tunisia";
+    para.classList.add("text");
+    para.innerHTML="We are located at  St 415 Med Ali  Gabes Tunisia";
     contact.appendChild(para);
-
+    const head2=document.createElement('h1');
+    head2.textContent="Phone Number";
+    contact.appendChild(head2);
+    const para2=  document.createElement('p');
+    para2.classList.add("text");
+    para2.innerHTML="+216 999 999";
+    contact.appendChild(para2);
      content.appendChild(contact);
 
  
--- src/menu.js
+++ src/menu.js
@@ -1,14 +1,68 @@
  export const menus= function () {
     const content=document.getElementById('content');
     const container= document.createElement('div');
-    container.classList.add("menu");
+    container.classList.add("imenu");
     content.appendChild(container);
-    const veget= document.createElement('div');
-    veget.classList.add("vegt");
-    veget.innerHTML=`VEGETEIESSSSSSSSSSS`
-    container.appendChild(veget);
-    const nonvegt= document.createElement('div');
-    nonvegt.classList.add("nonvegt");
-    nonvegt.innerHTML=`NOOOOOOOOOOOOOOOOOON VEGETEIESSSSSSSSSSS`
-    container.appendChild(nonvegt);
+    
+    
+   
+   const div1 = document.createElement('div');
+    div1.setAttribute('id', "d1");    
+    div1.classList.add('items');
+    const div11 = document.createElement('div');
+    div11.setAttribute('id', "d11");  
+    const h11 =document.createElement('h2');
+    h11.textContent="Fries Bowl";
+    const img11 = document.createElement("img");
+    img11.src = "/home/abdu/Project-Restaurant-Page/src/assets/friesbowl.png";
+   const p11 =document.createElement('p');
+    p11.textContent="590 - 1140 CAL"
+    div11.appendChild(h11); 
+    div11.appendChild(p11);
+     div11.appendChild(img11)
+    div1.appendChild(div11); 
+
+    container.appendChild(div1); 
+ 
+    //ITEM 2
+    const div2 = document.createElement('div')
+    div2.setAttribute('id', "d2");    
+    div2.classList.add('items');
+    const div22 = document.createElement('div');
+    div22.setAttribute('id', "d22");  
+    const h22 =document.createElement('h2');
+    h22.textContent="Zoodles Bowl";
+    const img22 = document.createElement("img");
+    img22.src = "/home/abdu/Project-Restaurant-Page/src/assets/ZoodlesBowl.png";
+   const p22 =document.createElement('p');
+    p22.textContent="590 - 1140 CAL"
+    div22.appendChild(h22); 
+    div22.appendChild(p22);
+     div22.appendChild(img22)
+    div2.appendChild(div22); 
+
+    container.appendChild(div2); 
+   
+    //ITEM 3
+    const div3 = document.createElement('div')
+    div3.setAttribute('id', "d3");    
+    div3.classList.add('items');
+    const div33 = document.createElement('div');
+    div33.setAttribute('id', "d33");  
+    const h33 =document.createElement('h2');
+    h33.textContent="Naanarito";
+    const img33 = document.createElement("img");
+    img33.src = "/home/abdu/Project-Restaurant-Page/src/assets/naanarito.png";
+   const p33 =document.createElement('p');
+    p33.textContent="590 - 1140 CAL"
+    div33.appendChild(h33); 
+    div33.appendChild(p33);
+     div33.appendChild(img33)
+    div3.appendChild(div33); 
+
+    container.appendChild(div3); 
+
+
+    
+   
 }
